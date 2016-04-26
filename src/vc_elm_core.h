fb8baca780c303822a602c3b012f41f60c739455
sooyeon.kim <sooyeon.kim@samsung.com>
2016-04-26 14:03:45 +0900

    check and revise by coding rule
    Change-Id: I29c42d825d73473692a7a75072d562ad64acbfb3
    Signed-off-by: sooyeon.kim <sooyeon.kim@samsung.com>

--- src/vc_elm_core.h
+++ src/vc_elm_core.h
@@ -31,8 +31,8 @@ extern "C" {
  * @brief This file contains core functions, that define the behaviour of library
  */
 
-//#define VC_ELM_CONFIG_XML 		VOICE_LIBDIR"/voice/vc-elm/1.0/voice-control-elm-config.xml"
-#define VC_ELM_CONFIG_XML 			tzplatform_mkpath(TZ_SYS_RO_SHARE, "voice/vc-elm/1.0/voice-control-elm-config.xml")
+//#define VC_ELM_CONFIG_XML		VOICE_LIBDIR"/voice/vc-elm/1.0/voice-control-elm-config.xml"
+#define VC_ELM_CONFIG_XML		tzplatform_mkpath(TZ_SYS_RO_SHARE, "voice/vc-elm/1.0/voice-control-elm-config.xml")
 #define VC_ELM_TAG_BASE_NAME		"voice-control-elm-config"
 
 #define VC_ELM_TAG_ACTION_LIST		"action-list"
